54cd7be721d6ea5a9295452f3617bdb615c85269
Gustavo <gustavo.silva1995@outlook.com>
2021-03-23 21:58:06 -0300

    Adicionar arquivos de projeto.

---
+++ BaltaStore.Domain/StoreContext/Entities/Delivery.cs
@@ -0,0 +1,20 @@
+using BaltaStore.Domain.StoreContext.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace BaltaStore.Domain.StoreContext.Entities
+{
+    public class Delivery
+    {
+        public Delivery(DateTime estimatedDeliveryDate)
+        {
+            CreateDate = DateTime.Now;
+            EstimatedDeliveryDate = estimatedDeliveryDate;
+            Status = EDeliveryStatus.Waiting;
+        }
+
+        public DateTime CreateDate { get; private set; }
+        public DateTime EstimatedDeliveryDate { get; private set; }
+        public EDeliveryStatus Status { get; private set; }
+    }
+}
